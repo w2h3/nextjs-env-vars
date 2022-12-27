f8321627c37c8416102a4608f38465e88608bde2
w2h3 <118594933+w2h3@users.noreply.github.com>
2022-12-27 17:21:33 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import { NextResponse } from 'next/server';
+import { get } from '@vercel/edge-config';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -120,4 +122,12 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+
+export const config = { matcher: '/welcome' };
+
+export async function middleware() {
+  const greeting = await get('greeting');
+  return NextResponse.json(greeting);
 }
